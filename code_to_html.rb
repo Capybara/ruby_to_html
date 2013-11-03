21be27c4817062353c93251d6929741b5f840452
Capybara <git.capybara@gmail.com>
2013-11-02 22:37:50 -0400

    fixed links

--- code_to_html.rb
+++ code_to_html.rb
@@ -7,16 +7,20 @@ require 'coderay'
 
 Dir.entries(Dir.pwd).each do |file|
 	if "#{File.extname(file)}" == '.rb'
+		(@arr ||= []) << file
+	end
+end
+	@arr.each_index do |i|
 		Dir.mkdir('Site', 0777) unless File.directory?('Site')
-		File.open("Site/#{file}.html",'w') do |fi|
-			fi << "<h1>#{file}</h1>"
-			fi << CodeRay.scan_file(file).div
+		File.open("Site/#{@arr[i]}.html",'w') do |fi|
+			fi << "<h1>#{@arr[i]}</h1>"
+			fi << CodeRay.scan_file(@arr[i]).div
 			fi << "<br>"
-			fi << "<a href='index.html'>Home</a>"
+			fi << "<a href='index.html'>Table of Contents</a></br></br>"
+			fi.puts "<a href=\"#{@arr[i + 1] || @arr[0]}.html\">Next: #{@arr[i + 1] || @arr[0]}</a></br>"
 			fi.close
 		end
 		File.open('Site/index.html','ab+') do |fi|
-			fi << "<a href=\"#{file}.html\">#{file}</a></br>"
+			fi.puts "<a href=\"#{@arr[i]}.html\">#{@arr[i]}</a></br>"
 		end
 	end
-end
