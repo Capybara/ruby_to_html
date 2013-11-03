65bd1bf4a6363bb18a4abc6de56e42fd58722fc7
Capybara <git.capybara@gmail.com>
2013-11-03 00:55:47 -0400

    check for Ruby files recursively

--- code_to_html.rb
+++ code_to_html.rb
@@ -5,22 +5,24 @@
 # ruby 2.0.0p247
 require 'coderay'
 
-Dir.entries(Dir.pwd).each do |file|
-	if "#{File.extname(file)}" == '.rb'
+Dir.glob("**/*.rb") do |file|
 		(@arr ||= []) << file
-	end
 end
 	@arr.each_index do |i|
 		Dir.mkdir('Site', 0777) unless File.directory?('Site')
-		File.open("Site/#{@arr[i]}.html",'w') do |fi|
-			fi << "<h1>#{@arr[i]}</h1>"
+		File.open("Site/#{File.basename(@arr[i])}.html",'w') do |fi|
+			fi << "<h1>#{File.basename(@arr[i])}</h1>"
 			fi << CodeRay.scan_file(@arr[i]).div
 			fi << "<br>"
 			fi << "<a href='index.html'>Table of Contents</a></br></br>"
-			fi.puts "<a href=\"#{@arr[i + 1] || @arr[0]}.html\">Next: #{@arr[i + 1] || @arr[0]}</a></br>"
+			if @arr[i + 1]
+				fi << "<a href=\"#{File.basename(@arr[i + 1])}.html\">Next: #{File.basename(@arr[i + 1])}</a></br>"
+			else
+				fi << "<a href=\"#{File.basename(@arr[0])}.html\">Next: #{File.basename(@arr[0])}</a></br>"
+			end
 			fi.close
 		end
 		File.open('Site/index.html','ab+') do |fi|
-			fi.puts "<a href=\"#{@arr[i]}.html\">#{@arr[i]}</a></br>"
+			fi.puts "<a href=\"#{File.basename(@arr[i])}.html\">#{File.basename(@arr[i])}</a></br>"
 		end
 	end
